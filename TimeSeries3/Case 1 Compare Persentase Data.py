4862df2661484bc7563232a8e3b7991918be5ce2
danny wahyudi <wahyudidanny23@gmail.com>
2018-12-22 23:22:41 +0700

    Update Case 1 Compare Persentase Data.py

--- TimeSeries3/Case 1 Compare Persentase Data.py
+++ TimeSeries3/Case 1 Compare Persentase Data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[5]:
 
 
 import pandas as pd 
@@ -25,7 +25,7 @@ plt.ylabel("Price") #to create label in x
 #plt.plot(dfBBCA,dfBBNI)
 
 
-# In[28]:
+# In[8]:
 
 
 meanBBCAfirst = BBCA['2018-01-01':"2018-01-31"].dropna(how='all').Close.mean()
@@ -35,7 +35,7 @@ valuepersetanseBBCA, "%.2f" % meanBBCAfirst, "%.2f" %  meanBBCAlast
 valuepersetanseBBCA
 
 
-# In[29]:
+# In[10]:
 
 
 meanBBNIfirst = BBNI['2018-01-01':"2018-01-31"].dropna(how='all').Close.mean()
@@ -45,7 +45,7 @@ valuepersetanseBBNI, "%.2f" % meanBBNIfirst,"%.2f" % meanBBNILast
 valuepersetanseBBNI
 
 
-# In[30]:
+# In[13]:
 
 
 meanBBRIfirst = BBRI['2018-01-01':"2018-01-31"].dropna(how='all').Close.mean()
@@ -55,7 +55,7 @@ valuepersetanseBBRI, "%.2f" % meanBBRIfirst,meanBBRIlast
 valuepersetanseBBRI
 
 
-# In[31]:
+# In[12]:
 
 
 meanBMRIfirst = BMRI['2018-01-01':"2018-01-31"].dropna(how='all').Close.mean()
@@ -65,32 +65,59 @@ valuepersetanseBMRI, "%.2f" % meanBMRIfirst,"%.2f" % meanBMRIlast
 valuepersetanseBMRI
 
 
-# In[36]:
+# In[14]:
 
 
 objects = ("BBCA","BBNI","BBRI","BMRI")
-y_pos = np.arange(len(x)) # untuk mengeset array yang ada pada X
+y_pos = np.arange(len(objects)) # untuk mengeset array yang ada pada X
 performance = [valuepersetanseBBCA, valuepersetanseBBNI, valuepersetanseBBRI, valuepersetanseBMRI]
-
 plt.bar(y_pos,performance, align='center', alpha=0.5)
 plt.xticks(y_pos,objects)
 plt.ylabel('Persentase') #label untuk Y
 plt.xlabel('Stock Banking Indonesia')#label untuk X
 plt.title('Persentase Kenaikan Harga Stock Banking Diindonesia 2018')
+plt.minorticks_on()
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='red')
 plt.show()
 
-#sebelumnya tidak berjalan karena value dibuat menjadi string
-#plt.title('Grafik Bar Persentase Kenaikan')
-#plt.xlabel('Stock Finance Indonesia')
-#plt.ylabel('Persentase Kenaikan')
-#plt.bar(x, y, align='center', alpha=0.5)
-#plt.show()
+
+# In[25]:
 
 
-# In[ ]:
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+N = 5
+men_means = (54.74, 42.35, 67.37, 58.24, 30.25)
+men_std= (4, 3, 4, 1, 5)
+
+ind = np.arange(N)  # the x locations for the groups
+width = 0.35       # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind, men_means, width, color='r', yerr=men_std)
+
+# add some text for labels, title and axes ticks
+plt.ylabel('Scores')
+plt.xlabel('Velocity')
+plt.title('Scores by Velocity')
+
+red_patch = mpatches.Patch(color='red', label='Men')
+plt.legend(handles=[red_patch])
 
 
-
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+       height = rect.get_height()
+       ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+       ha='center', va='bottom')
+autolabel(rects1)
+plt.show()
 
 
 # In[ ]:
